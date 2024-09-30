fa657e77508bc5d840b58ee4f087e81922a13905
Shalom Rochman <shalom.rochman@high-rad.com>
2024-09-30 17:36:51 +0300

    documentation of the typings in algo_typing.py

--- algo_typing.py
+++ algo_typing.py
@@ -1,5 +1,5 @@
 from typing import Tuple, List
 
-CC = Tuple[List[int], List[int]]
-IndexExpression3D = Tuple[slice, slice, slice]
-VoxelSpacing = Tuple[float, float, float]
+CC = Tuple[List[int], List[int]] # Connected Component (CC) of labeled lesions
+IndexExpression3D = Tuple[slice, slice, slice] # Index Expression for 3D numpy array slicing (x, y, z)
+VoxelSpacing = Tuple[float, float, float] # Voxel spacing in x, y, z direction (mm)
