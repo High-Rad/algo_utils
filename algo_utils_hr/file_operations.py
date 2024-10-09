b774cfe7cee8fcf6f765a6866ed23e480465d3d0
Shalom Rochman <shalom.rochman@high-rad.com>
2024-10-09 16:12:07 +0300

    adding get_project_root and get_absolute_path functions to file_operations.py

--- algo_utils_hr/file_operations.py
+++ algo_utils_hr/file_operations.py
@@ -2,6 +2,7 @@
 Algorithm Utilities: This module contains various functions for file operations.
 """
 
+import inspect
 import json
 import os
 from glob import glob
@@ -13,7 +14,7 @@ from nibabel import load, as_closest_canonical, Nifti1Image
 
 
 __all__ = ['load_nifti_data', 'replace_in_file_name', 'symlink_for_inner_files_in_a_dir',
-           'load_and_validate_jsonschema']
+           'load_and_validate_jsonschema', 'get_project_root', 'get_absolute_path']
 
 
 def load_nifti_data(nifti_file_name: str) -> Tuple[np.ndarray, Nifti1Image]:
@@ -162,3 +163,88 @@ def load_and_validate_jsonschema(json_fn: str, json_format: dict) -> dict:
         raise jsonschema.exceptions.ValidationError(
             f"Validation error in JSON file '{json_fn}': {e.message}"
         ) from e
+
+
+def get_project_root(project_marker_file_basename: str = "project_marker.txt", stack_index: int = 1) -> str:
+    """
+    Returns the project's root directory by looking for a marker file in the caller function's project.
+
+    Notes
+    -----
+    This function is useful when the caller function is located in the project's subdirectory, and the project marker
+    file is located in the project's root directory. The function traverses the directory tree upwards until it finds
+    the project marker file. If the project marker file is not found, a FileNotFoundError is raised. The project marker
+    file is assumed to be named "project_marker.txt" by default. The name of the project marker file can be changed by
+    passing the `project_marker_file_basename` parameter.
+
+    Parameters
+    ----------
+    project_marker_file_basename : str
+        The name of the project marker file. Default is "project_marker.txt".
+    stack_index : int
+        The index of the stack frame to get the file path from. Default is 1. If one wants to get the project root of
+        the function that called the function that called this function, the index should be 2, and so on.
+
+    Returns
+    -------
+    str
+        The project's root directory.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the project marker file is not found in the caller function's project.
+    """
+
+    if stack_index < 1:
+        raise IndexError("The stack index should be greater than or equal to 1.")
+
+    stack = inspect.stack()
+
+    if stack_index >= len(stack):
+        raise IndexError("The stack index is out of range. Namely, it is greater than the number of stack frames.")
+
+    caller_function_file_abs_path = os.path.abspath(stack[stack_index].filename)
+
+    directory = os.path.dirname(caller_function_file_abs_path)
+    while directory != "/":  # Stop when reached the root directory
+        if project_marker_file_basename in os.listdir(directory):
+            return directory
+        directory = os.path.dirname(directory)
+    raise FileNotFoundError(f"Project marker file ({project_marker_file_basename}) not found. "
+                            f"Ensure you have a marker file in the project root.")
+
+
+def get_absolute_path(p: str, project_marker_file_basename: str = "project_marker.txt") -> str:
+    """
+    Returns an absolute path of a file/dir, assuming it's located in the caller function's project.
+
+    Notes
+    -----
+    This function is useful when the caller function is located in the project's subdirectory, and the project marker
+    file is located in the project's root directory. The function traverses the directory tree upwards until it finds
+    the project marker file. If the project marker file is not found, a FileNotFoundError is raised. The project marker
+    file is assumed to be named "project_marker.txt" by default. The name of the project marker file can be changed by
+    passing the `project_marker_file_basename` parameter.
+
+    Parameters
+    ----------
+    p : str
+        Relative path to a file/dir located in the current project. The path should be relative to the project's root
+        directory.
+    project_marker_file_basename : str
+        The name of the project marker file. Default is "project_marker.txt".
+
+    Returns
+    -------
+    str
+        Absolute path to the given file/dir (assuming it's located in the caller function's project).
+
+    Raises
+    ------
+    FileNotFoundError
+        If the project marker file is not found in the caller function's project.
+    """
+    if p.startswith('/'):
+        return get_absolute_path(p[1:])
+    return os.path.join(get_project_root(project_marker_file_basename, 2), p)
